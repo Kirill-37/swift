3156a11bb1ac6c6f4e25dad467b0f50a21b13b4c
Kirill Kharuzin <k.kharuzin@gmail.com>
2019-10-10 11:40:26 +0300

    внес правки по первым двум функциям

--- lesson2/lesson2.playground/Contents.swift
+++ lesson2/lesson2.playground/Contents.swift
@@ -1,32 +1,33 @@
 import UIKit
 
 // Функция определения четного числа
-/*var evenNumber = [Int] ()
-for i in 0...100{
-    if i % 2 == 0{
-        evenNumber.append(i)
-    }
+
+/*var evensNumber = [Int]()
+func isEven(number: Int) -> Bool {
+  return number % 2 == 0
 }
-print(evenNumber)*/
+evensNumber = Array(1...3).filter(isEven)
+print(evensNumber)*/
+
+
 
 // Функция определения делится ли число на 3
 
-/*var treeNumber = [Int]()
-for i in 0...100{
-    if i % 3 == 0{
-        treeNumber.append(i)
-    }
+/*var threeNumber = [Int]()
+func isThree(number: Int) -> Bool {
+  return number % 3 == 0
 }
-print(treeNumber)*/
+threeNumber = Array(1...3).filter(isThree)
+print(threeNumber)*/
 
 // Создание возрастающего массива
 
-var array = [Int](0...100)
+/*var array = [Int](0...100)
 
 // Удаление всех четных и чисел, которые делятся на 3
 
-array.removeAll(where: { $0 % 2 == 0 || $0 % 3 == 0} )
-print(array)
+array.removeAll(where: { $0 % 2 == 0 || $0 % 3 > 0} )
+print(array)*/
 
 
 
