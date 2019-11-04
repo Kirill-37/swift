6a2946505a8040a01ec4238ff93f303f4447dfec
Kirill Kharuzin <k.kharuzin@gmail.com>
2019-11-05 00:46:15 +0300

    змейка

---
+++ lesson8/Snake_Kir/Snake_KirTests/Snake_KirTests.swift
@@ -0,0 +1,34 @@
+//
+//  Snake_KirTests.swift
+//  Snake_KirTests
+//
+//  Created by Кирилл Харузин on 04/11/2019.
+//  Copyright © 2019 Кирилл Харузин. All rights reserved.
+//
+
+import XCTest
+@testable import Snake_Kir
+
+class Snake_KirTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
